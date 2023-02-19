5154d9ef4ed59e67ccc4c4af3c12c488b6c94443
Bulat <beast8357@mail.ru>
2023-02-19 22:57:15 +0300

    fix: recreate 'answer' model

--- db/migrate/20230219195626_create_answers.rb
+++ db/migrate/20230219195626_create_answers.rb
@@ -2,7 +2,7 @@ class CreateAnswers < ActiveRecord::Migration[6.1]
   def change
     create_table :answers do |t|
       t.string :body
-      t.boolean :result
+      t.boolean :correct
 
       t.timestamps
     end
