9391420791635605dd72b1439d9b13b002f2f071
Bulat <beast8357@mail.ru>
2023-02-22 14:05:30 +0300

    feat: add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,82 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+users = User.create(
+  [
+    { name: "Alex", email: "alex228@jail.com" },
+    { name: "Zelda", email: "deffka@jail.com" },
+    { name: "Sam", email: "serious@jail.com" },
+  ]
+)
+
+categories = Category.create(
+  [
+    { title: "Programming" },
+    { title: "Music Mixing" },
+  ]
+)
+
+tests = Test.create(
+  [
+    {
+      title: "Ruby Basics",
+      level: 1,
+      category_id: Category.find_by!(title: "Programming").id
+    },
+    {
+      title: "Ruby Intermediate",
+      level: 2,
+      category_id: Category.find_by!(title: "Programming").id
+    },
+    {
+      title: "Mixing Basics",
+      level: 1,
+      category_id: Category.find_by!(title: "Music Mixing").id
+    },
+    {
+      title: "Mixing Advanced",
+      level: 3,
+      category_id: Category.find_by!(title: "Music Mixing").id
+    },
+  ]
+)
+
+questions = Question.create(
+  [
+    {
+      body: "Does a method definition start with the word \'def\'?",
+      test_id: Test.find_by!(title: "Ruby Basics").id
+    },
+    {
+      body: "Can you call private methods from other classes?",
+      test_id: Test.find_by!(title: "Ruby Basics").id
+    },
+    {
+      body: "Do \'proc\' and \'lambda\' act the same way?",
+      test_id: Test.find_by!(title: "Ruby Intermediate").id
+    },
+    {
+      body: "Is everything an object in Ruby?",
+      test_id: Test.find_by!(title: "Ruby Intermediate").id
+    },
+    {
+      body: "First you do mixing and only then mastering",
+      test_id: Test.find_by!(title: "Mixing Basics").id
+    },
+    {
+      body: "Equalizers are used to compress the audio signal",
+      test_id: Test.find_by!(title: "Mixing Basics").id
+    },
+    {
+      body: "Parallel compression is when you put the audio " \
+            "signal through several compressors on different channels",
+      test_id: Test.find_by!(title: "Mixing Advanced").id
+    },
+    {
+      body: "On the mix bus it\'s best to use an EQ in zero-latency mode",
+      test_id: Test.find_by!(title: "Mixing Advanced").id
+    },
+  ]
+)
+
+questions.each.with_index(1) do |question, index|
+  Answer.create(body: "Yes", correct: true, question_id: question.id)
+  Answer.create(body: "No", correct: false, question_id: question.id)
+end
