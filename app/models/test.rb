af1fd4f682e7f707b9591b78f8eda1dca8a3fa21
Bulat <beast8357@mail.ru>
2023-02-22 16:12:00 +0300

    feat: add an instance method to User and a class method to Test

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,2 +1,11 @@
 class Test < ApplicationRecord
+  belongs_to :category
+  has_many :user_tests
+  has_many :users, through: :user_tests
+
+  class << self
+    def sort_by_category(name)
+      joins(:category).where("categories.title = ?", name).order(title: :DESC).pluck("tests.title")
+    end
+  end
 end
