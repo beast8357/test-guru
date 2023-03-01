51a1ce0fb3063d7673697fdadd3b547c31183399
Bulat <beast8357@mail.ru>
2023-03-01 13:53:50 +0300

    feat: add creator to tests

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,5 +1,6 @@
 class Test < ApplicationRecord
   belongs_to :category
+  belongs_to :creator, class_name: "User", foreign_key: "creator_id"
   has_many :questions
   has_many :users_tests
   has_many :users, through: :users_tests
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_01_095221) do
+ActiveRecord::Schema.define(version: 2023_03_01_103008) do
 
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
@@ -41,7 +41,9 @@ ActiveRecord::Schema.define(version: 2023_03_01_095221) do
     t.integer "category_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "creator_id", null: false
     t.index ["category_id"], name: "index_tests_on_category_id"
+    t.index ["creator_id"], name: "index_tests_on_creator_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -63,6 +65,7 @@ ActiveRecord::Schema.define(version: 2023_03_01_095221) do
   add_foreign_key "answers", "questions"
   add_foreign_key "questions", "tests"
   add_foreign_key "tests", "categories"
+  add_foreign_key "tests", "users", column: "creator_id"
   add_foreign_key "users_tests", "tests"
   add_foreign_key "users_tests", "users"
 end
