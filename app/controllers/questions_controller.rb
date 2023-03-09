924b5218690ba1b1d8a402bd819b4ed4de1e2b84
Bulat <beast8357@mail.ru>
2023-03-09 22:24:34 +0300

    add: display of a specific test question

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,16 +1,25 @@
 class QuestionsController < ApplicationController
 
-  before_action :find_test, only: %i[index]
+  before_action :find_test, only: %i[index, show]
+  before_action :find_question, only: %i[show]
 
   def index
     test_questions = @test.questions.pluck(:body)
     render plain: "Test#{@test.id} questions:\n#{test_questions.join("\n")}"
   end
 
+  def show
+    render plain: "Question body:\n#{@question.body}"
+  end
+
   private
 
   def find_test
     @test = Test.find(params[:test_id])
+  end
+
+  def find_question
+    @question = @test.questions.find(params[:id])
   end
 
 end
