15aa801c0c5489b3c919386a57008f3604b9fa7c
Bulat <beast8357@mail.ru>
2023-03-22 13:51:14 +0300

    feat: create helpers and refactor footer

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+
+  def current_year
+    "Год: #{Time.current.year}"
+  end
+
+  def github_url(author, repo)
+    link_to "GitHub", "https://github.com/#{author}/#{repo}"
+  end
+
+  def school_url
+    link_to "Учебный проект в онлайн-школе Thinknetica",
+            "https://thinknetica.com/",
+            target: :blank
+  end
+
 end
