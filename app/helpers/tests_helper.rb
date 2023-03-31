53b482c6653cc226493d3730dfbf45422b0f8f03
Bulat <beast8357@mail.ru>
2023-03-31 16:35:27 +0300

    fix: answers & level-displaying scopes in the ' Test' model

--- app/helpers/tests_helper.rb
+++ app/helpers/tests_helper.rb
@@ -1,9 +1,9 @@
 module TestsHelper
 
-  TEST_LEVELS = { 1 => 'Easy', 2 => 'Normal', 3 => 'Hard' }.freeze
+  TEST_LEVELS = { 1 => 'Easy', 2 => 'Medium', 3 => 'Hard' }.freeze
 
   def test_level(test)
-    TEST_LEVELS[test.level] || 'Hero'
+    TEST_LEVELS[test.level]
   end
 
 end
--- app/models/test.rb
+++ app/models/test.rb
@@ -10,7 +10,7 @@ class Test < ApplicationRecord
 
   scope :by_category, -> (name) { joins(:category).where("categories.title = ?", name) }
   scope :by_level, -> (level) { where(level: level) }
-  scope :easy, -> { by_level(0..1) }
-  scope :medium, -> { by_level(2..4) }
-  scope :hard, -> { by_level(5..Float::INFINITY) }
+  scope :easy, -> { by_level(1) }
+  scope :medium, -> { by_level(2) }
+  scope :hard, -> { by_level(3) }
 end
