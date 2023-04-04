9f3b41f8f432d578ec6fa940456b2bac4170bc5c
Bulat <beast8357@mail.ru>
2023-04-04 20:01:25 +0300

    feat: create the 'start' action in the tests controller and create the 'test_passage' method in the 'user' model

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,6 +1,7 @@
 class TestsController < ApplicationController
 
-  before_action :find_test, only: %i[ show ]
+  before_action :set_test, only: %i[ start show ]
+  before_action :set_user, only: :start
 
   def index
     @tests = Test.all
@@ -10,10 +11,20 @@ class TestsController < ApplicationController
     @test_questions = @test.questions.pluck(:body)
   end
 
+  def start
+    @user.tests.push(@test)
+
+    redirect_to @user.test_passage(@test)
+  end
+
   private
 
-  def find_test
+  def set_test
     @test = Test.find(params[:id])
+  end
+
+  def set_user
+    @user = User.first
   end
 
 end
