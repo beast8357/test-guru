73dce81ee60319ef9ed41051c7c8082613b288bb
Bulat <beast8357@mail.ru>
2023-04-08 12:56:59 +0300

    feat: implement the assignment of the next question using a callback & change the way to determine if the answer is correct

--- app/models/test_passage.rb
+++ app/models/test_passage.rb
@@ -1,10 +1,11 @@
 class TestPassage < ApplicationRecord
-  
+
   belongs_to :user
   belongs_to :test
   belongs_to :current_question, class_name: "Question", optional: true
 
   before_validation :before_validation_set_first_question, on: :create
+  before_update :before_update_set_next_question
 
   def completed?
     current_question.nil?
@@ -12,7 +13,6 @@ class TestPassage < ApplicationRecord
 
   def accept!(answer_ids)
     self.correct_questions += 1 if correct_answer?(answer_ids)
-    self.current_question = next_question
     save!
   end
 
@@ -23,14 +23,15 @@ class TestPassage < ApplicationRecord
   end
 
   def correct_answer?(answer_ids)
-    correct_answers_count = correct_answers.count
-
-    (correct_answers_count == correct_answers.where(id: answer_ids).count) &&
-    correct_answers_count == answer_ids.count
+    correct_answers.ids.sort == answer_ids.map(&:to_i).sort
   end
 
   def correct_answers
     current_question.answers.correct
+  end
+
+  def before_update_set_next_question
+    self.current_question = next_question
   end
 
   def next_question
