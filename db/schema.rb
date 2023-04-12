7c63ee6c1a7138ecb552abad26d0ba7ebc559796
Bulat <beast8357@mail.ru>
2023-04-12 19:40:49 +0300

    add password digest to the 'users' table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_04_04_164042) do
+ActiveRecord::Schema.define(version: 2023_04_12_163905) do
 
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
@@ -63,6 +63,7 @@ ActiveRecord::Schema.define(version: 2023_04_04_164042) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "name", null: false
+    t.string "password_digest"
   end
 
   add_foreign_key "answers", "questions"
