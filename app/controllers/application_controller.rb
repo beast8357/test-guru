3479657a5be8510f69a9cefd2c5d699544471bb3
Bulat <beast8357@mail.ru>
2023-04-16 16:07:26 +0300

    refactor the 'authenticate_user!' callback

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,9 +7,7 @@ class ApplicationController < ActionController::Base
   private
 
   def authenticate_user!
-    unless current_user
-      redirect_to login_path
-    end
+    redirect_to login_path unless current_user
   end
 
   def current_user
