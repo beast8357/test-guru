4488d1f9722cf098a782e5eba86a9232849d3c2f
Bulat <beast8357@mail.ru>
2023-04-17 18:12:44 +0300

    add a flash message display when logging in with incorrect login details

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,6 +11,7 @@ class SessionsController < ApplicationController
       session[:user_id] = user.id
       redirect_to tests_path
     else
+      flash[:alert] = 'Please, verify your email and password'
       render :new
     end
   end
