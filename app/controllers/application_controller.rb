a217c076ed83e3c5a4806552a4e2d5a71eed1356
Bulat <beast8357@mail.ru>
2023-04-17 18:29:42 +0300

    add a flash message display when trying to navigate the app without logging in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,13 @@ class ApplicationController < ActionController::Base
 
   protect_from_forgery with: :exception
 
-  helper_method :current_user
+  helper_method :current_user,
+                :logged_in?
 
   private
 
   def authenticate_user!
-    redirect_to login_path unless current_user
+    redirect_to login_path, alert: 'Please, verify your email and password' unless current_user
   end
 
   def current_user
