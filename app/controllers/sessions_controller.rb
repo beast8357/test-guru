4f33b751ed67d6c92e49771ae3b98ed47d45fcb1
Bulat <beast8357@mail.ru>
2023-04-25 18:56:53 +0300

    add a different style for the message when the user logs out

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -20,7 +20,7 @@ class SessionsController < ApplicationController
 
   def destroy
     session[:user_id] = nil
-    redirect_to login_path, alert: 'You are logged out'
+    redirect_to login_path, notice: 'You are logged out'
   end
 
 end
