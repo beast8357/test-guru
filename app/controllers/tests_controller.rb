ca67d8971c3a1aa98cc2f1c8b50c9955b032635f
Bulat <beast8357@mail.ru>
2023-04-26 14:05:41 +0300

    use the current user object when starting the test

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,7 +1,6 @@
 class TestsController < ApplicationController
 
   before_action :set_test, only: %i[start show]
-  before_action :set_user, only: :start
 
   def index
     @tests = Test.all
@@ -16,19 +15,15 @@ class TestsController < ApplicationController
   end
 
   def start
-    @user.tests.push(@test)
+    current_user.tests.push(@test)
 
-    redirect_to test_passage_path(@user.test_passage(@test))
+    redirect_to test_passage_path(current_user.test_passage(@test))
   end
 
   private
 
   def set_test
     @test = Test.find(params[:id])
-  end
-
-  def set_user
-    @user = User.first
   end
 
 end
