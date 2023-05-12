74394d34052ca7f8d8acd83afbfdabdee090a9f4
Bulat <beast8357@mail.ru>
2023-05-12 16:06:30 +0300

    fix: make the app layout compatible with devise

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   root to: "tests#index"
 
-  devise_for :users, path: :gurus, path_names: { sign_up: :login, sign_out: :logout }
+  devise_for :users, path: :gurus, path_names: { sign_in: :login, sign_out: :logout }
 
   resources :tests do
     resources :questions do
