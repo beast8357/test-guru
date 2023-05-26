70351c4a99eb9d3efbea2ac489292293c2bac618
Bulat <beast8357@mail.ru>
2023-05-26 12:21:41 +0300

    feat: add an admin redirect to the /admin/tests page after logging in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,10 @@ class ApplicationController < ActionController::Base
 
   protect_from_forgery with: :exception
 
+  private
+
+  def after_sign_in_path_for(resource)
+    current_user.admin? ? admin_tests_path : root_path
+  end
+
 end
