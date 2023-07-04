10993d960a3c331cbc38d33dcff5e5fa5c2ae576
Bulat <beast8357@mail.ru>
2023-07-04 13:41:37 +0300

    refactor: remove brackets from the default locale key

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,6 @@ module TestGuru
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
     config.time_zone = 'Moscow'
-    config.i18n.default_locale = :'en'
+    config.i18n.default_locale = :en
   end
 end
