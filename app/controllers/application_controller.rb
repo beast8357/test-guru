6e4cf2180b30a93ae043df725b9e3ac07491d2cd
Bulat <beast8357@mail.ru>
2023-07-04 14:32:04 +0300

    fix: syntax errors in application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,7 +21,7 @@ class ApplicationController < ActionController::Base
   end
 
   def set_locale
-    I18n.locale = I18n.locale_available?(params[:lang]) : params[:lang] || I18n.default_locale
+    I18n.locale = I18n.locale_available?(params[:lang]) ? params[:lang] : I18n.default_locale
   end
 
 end
