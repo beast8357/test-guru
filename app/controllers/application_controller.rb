1cae830b2c8eb350ea5252bba53c0a46710d42f7
Bulat <beast8357@mail.ru>
2023-07-04 13:59:10 +0300

    feat: set the locale to be passed to the url

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!
+  before_action :set_locale
 
   protect_from_forgery with: :exception
 
@@ -13,6 +14,14 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+  end
+
+  def default_url_options
+    { lang: I18n.locale }
+  end
+
+  def set_locale
+    I18n.locale = I18n.locale_available?(params[:lang]) : params[:lang] || I18n.default_locale
   end
 
 end
