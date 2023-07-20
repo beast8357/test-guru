8cb66051ca9e7991e76fabb5a86007f38b10ce4c
Bulat <beast8357@mail.ru>
2023-07-20 18:26:01 +0300

    feat: add associations for gists

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,7 @@ class User < ApplicationRecord
   has_many :test_passages
   has_many :tests, through: :test_passages
   has_many :created_tests, class_name: "Test", foreign_key: "creator_id"
+  has_many :gists
 
   validates :first_name, presence: true
   validates :last_name, presence: true
