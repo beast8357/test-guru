e4539e055a2662642b394af1af51794f8d74fd83
Bulat <beast8357@mail.ru>
2023-09-02 13:26:01 +0300

    feat: prohibit the transition to the next question without selecting the answer to the current question

--- app/controllers/test_passages_controller.rb
+++ app/controllers/test_passages_controller.rb
@@ -11,6 +11,9 @@ class TestPassagesController < ApplicationController
   end
 
   def update
+    return redirect_to test_passage_path(@test_passage),
+                       alert: t('.no_answer') unless params[:answer_ids]
+
     @test_passage.accept!(params[:answer_ids])
 
     if @test_passage.completed?
