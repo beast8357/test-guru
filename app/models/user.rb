52febf59421a683839489b6a929d3cb65269b770
Bulat <beast8357@mail.ru>
2023-09-03 12:58:44 +0300

    fix: add deleting gists when destroying questions, and deleting test_passages when destroying a user

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
          :rememberable,
          :validatable
 
-  has_many :test_passages
+  has_many :test_passages, dependent: :destroy
   has_many :tests, through: :test_passages
   has_many :created_tests, class_name: "Test", foreign_key: "creator_id"
   has_many :gists
