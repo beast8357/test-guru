03bd57a2052e924e6c7a18d0cd38311df7cd2b09
Bulat <beast8357@mail.ru>
2023-09-17 19:18:36 +0300

    feat: add the 'description' attribute to badges

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_09_14_134818) do
+ActiveRecord::Schema.define(version: 2023_09_17_161645) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 2023_09_14_134818) do
     t.string "criterion", default: "", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "description", default: "", null: false
   end
 
   create_table "categories", force: :cascade do |t|
