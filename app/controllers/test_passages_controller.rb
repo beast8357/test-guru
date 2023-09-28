3a092638dc6b9060e704e1c1ef1fe35d8e91a2a1
Bulat <beast8357@mail.ru>
2023-09-28 12:12:20 +0300

    feat: give the user a badge once they pass their first test

--- app/controllers/test_passages_controller.rb
+++ app/controllers/test_passages_controller.rb
@@ -18,6 +18,7 @@ class TestPassagesController < ApplicationController
 
     if @test_passage.completed?
       TestsMailer.completed_test(@test_passage).deliver_now
+      create_badge! if user_passed_their_first_test?
       redirect_to result_test_passage_path(@test_passage)
     else
       render :show
@@ -28,6 +29,23 @@ class TestPassagesController < ApplicationController
 
   def set_test_passage
     @test_passage = TestPassage.find(params[:id])
+  end
+
+  def user_passed_their_first_test?
+    if current_user.test_passages.one?
+      current_user.test_passages.last.completed?
+    else
+      false
+    end
+  end
+
+  def create_badge!
+    current_user.badges.create!(
+        name: "First Test",
+        image_name: "first_test.png",
+        criterion: "Pass your first test",
+        description: "This badge is given to you once you pass your first test"
+      )
   end
 
 end
