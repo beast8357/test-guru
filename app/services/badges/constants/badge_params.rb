cd1ea2a4a4d9ae883ba9faae4bc069844d07daa8
Bulat <beast8357@mail.ru>
2023-10-24 13:37:28 +0300

    correct grammar in badges descriptions

--- app/services/badges/constants/badge_params.rb
+++ app/services/badges/constants/badge_params.rb
@@ -7,7 +7,7 @@ module Badges
         user_passed_their_first_test:
           { name: "First Test",
             image_name: "first_test.png",
-            description: "This badge is given to you once you pass your first test" },
+            description: "You passed your first test" },
         user_passed_the_test_flawlessly:
           { name: "Flawless Test",
             image_name: "flawless_test.png",
@@ -19,31 +19,31 @@ module Badges
         user_passed_the_test_flawlessly_on_the_first_attempt:
           { name: "Amazing First Attempt",
             image_name: "amazing_first_attempt.png",
-            description: "You have successfully passed the test on the first attempt" },
+            description: "You successfully passed the test on the first attempt" },
         user_successfully_passed_all_tests_on_programming:
           { name: "Programming Guru",
             image_name: "programming_guru.png",
-            description: "You have successfully passed all the tests from the 'Programming' category" },
+            description: "You successfully passed all the tests from the 'Programming' category" },
         user_successfully_passed_all_tests_on_music_mixing:
           { name: "Music Mixing Guru",
             image_name: "music_mixing_guru.png",
-            description: "You have successfully passed all the tests from the 'Music Mixing' category" },
+            description: "You successfully passed all the tests from the 'Music Mixing' category" },
         user_successfully_passed_all_tests_on_gaming:
           { name: "Gaming Guru",
             image_name: "gaming_guru.png",
-            description: "You have successfully passed all the tests from the 'Gaming' category" },
+            description: "You successfully passed all the tests from the 'Gaming' category" },
         user_successfully_passed_all_tests_lvl_easy:
           { name: "Easy Peasy",
             image_name: "easy_peasy.png",
-            description: "You have successfully passed all the tests with level 'Easy'" },
+            description: "You successfully passed all the tests with level 'Easy'" },
         user_successfully_passed_all_tests_lvl_medium:
           { name: "Medium Rare",
             image_name: "medium_rare.png",
-            description: "You have successfully passed all the tests with level 'Medium'" },
+            description: "You successfully passed all the tests with level 'Medium'" },
         user_successfully_passed_all_tests_lvl_hard:
           { name: "Die Hard",
             image_name: "die_hard.png",
-            description: "You have successfully passed all the tests with level 'Hard'" },
+            description: "You successfully passed all the tests with level 'Hard'" },
       }.freeze
     end
   end
